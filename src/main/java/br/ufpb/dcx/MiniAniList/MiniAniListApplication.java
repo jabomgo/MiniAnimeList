b8974b8f0d7e0ec8d1d7ffd184775cee0fe3ff8d
jabomgo <jonathan.neves@dcx.ufpb.br>
2025-09-03 21:54:04 -0300

    Inicializando o projeto, resolvendo dependencias e banco

---
+++ src/main/java/br/ufpb/dcx/MiniAniList/MiniAniListApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.MiniAniList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAniListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAniListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/dcx/MiniAniList/MiniAniListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.MiniAniList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAniListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
