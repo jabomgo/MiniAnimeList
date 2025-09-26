cb5defe9528f75501942519280330a8b7b3e14ee
jabomgo <jonathan.neves@dcx.ufpb.br>
2025-09-26 10:01:36 -0300

    Adicionado o Bcrypt

--- src/main/java/br/ufpb/dcx/MiniAniList/MiniAniListApplication.java
+++ src/main/java/br/ufpb/dcx/MiniAniList/MiniAniListApplication.java
@@ -2,11 +2,18 @@ package br.ufpb.dcx.MiniAniList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MiniAniListApplication {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 		SpringApplication.run(MiniAniListApplication.class, args);
 	}
 
